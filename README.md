f56cd7bae43d1b62274906f360f7bfcff4d2b5f1
Isaak Morales <ice.isaak97@gmail.com>
2022-03-29 21:44:12 -0400

    Finished my ReadMe and got the appending on the page

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Weather-Dashboard
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+My motivation for this project was to build an app that can display your weather forecast. I built this project to help people look for weather stats on where they want to go or just to see at there location. I learned how to use Jquery more and appending but still need to work on it.
+
+## Installation
+
+No installation required. All you need is to load in the page and input your city/location.
+
+## Usage
+
+Type in a location and your weather should appear.
+
+
+![alt = Weather Dashboard](./images/_C__Users_isaak_Desktop_homework_Weather-Dashboard_index.html%20(1).png)
+
+
+## Credits
+
+Kyle - https://github.com/kgiunta
+
+Dom - https://github.com/dev-dominick
+
+Scott McAnally - Tutor
+
+## License
+
+N/A
+
+## Badges
+
+N/A
+
+## Features
+
+N/A
+
+## How to Contribute
+
+N/A
+
+## Tests
+
+N/A
--- java/script.js
+++ java/script.js
@@ -24,31 +24,59 @@
 //TODO THEN I am again presented with current and future conditions for that city
 
 let APIKey = "8a1a83896610616dfd6f338bc91b14a2";
+let apiURL = "https://api.openweathermap.org";
 let button = $(".btn-primary");
 let cityName = $("#cityName");
 let date = $("#Date");
 let temp = $("#Temperature");
 let wind = $("#windSpeed");
 let uvIndex = $("#uvIndex");
-let userInput = $(".form-control");
+let userInput = $("#userInput");
 let history = [];
 
 function getApi(event) {
     event.preventDefault();
-    console.log("submit");
+    console.log("click");
     
     
-    let requestUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-    console.log(userInput.value);
+    let requestUrl = `${apiURL}/geo/1.0/direct?q=${userInput.val()}&units=imperial&appid=${APIKey}`;
+    console.log(userInput.val());
     fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-    .then(function (weather) {
-        console.log(weather);
-    
-     })
+        .then((response) => response.json())
+        .then((weather) => { 
+            console.log(weather[0])
+            oneCall(weather[0].lat, weather[0].lon)})
 };
-
-
 button.on("click", getApi)
+
+
+
+function oneCall(lat, lon) {
+     let oneCallURL = `${apiURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`
+    console.log(oneCallURL);
+     fetch(oneCallURL)
+
+     .then((response) => response.json())
+     .then ((data) => {
+         console.log(data);
+
+
+        $("#location").append(userInput.val())
+        $("#currentTemp").append(data.current.temp);
+        $("#currentUVI").append(data.current.uvi);
+        $("#currentWind").append(data.current.wind_speed);
+         console.log(data.current.temp);
+         console.log(data.current.uvi);
+         console.log(data.current.wind_speed);
+        
+     
+     
+     
+        })
+}
+
+
+
+// function fiveCall() {
+//     let fiveDayCall = `$`
+// }
